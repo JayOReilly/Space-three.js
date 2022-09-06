bf4061e7534a52e29bcc4dcb419ecf8e257c9a8e
JayOReilly <99806112+JayOReilly@users.noreply.github.com>
2022-09-06 22:04:35 +0100

    2nd commit - roating orbit

---
+++ vanilla3d/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const app = () => {
+  return (
+    <div>
+
+    <main />
+
+    </div>
+  )
+}
+
+export default app
--- vanilla3d/main.js
+++ vanilla3d/main.js
@@ -3,9 +3,10 @@ import * as THREE from 'three';
 import { render } from '@react-three/fiber';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import CHROME from '../vanilla3d/assets/chrome.jpg';
+import { Stars } from '@react-three/drei';
+
 
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
@@ -14,18 +15,22 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(150);
-
+camera.position.setZ(200);
 renderer.render(scene, camera);
 
-const geom = new THREE.SphereGeometry(50,40,50,1);
-const mats = new THREE.MeshBasicMaterial({ color: 0x91a8d0});
+
+
+
+/////////////////////////////////////////////
+
+
+const geom = new THREE.SphereGeometry(50,40,75);
+const mats = new THREE.MeshStandardMaterial
+    ({ });
 const ball = new THREE.Mesh(geom, mats);
-
-const texture1 = new THREE.TextureLoader().load('chrome.jpg')
-scene.ball = texture1;
-
 scene.add(ball)
+
+////////////////////////////////////
 
 const pointingLight = new THREE.PointLight(0xffffff)
 pointingLight.position.set(20,20,20)
@@ -33,44 +38,49 @@ pointingLight.position.set(20,20,20)
 scene.add(pointingLight)
 
 const control = new OrbitControls(camera, renderer.domElement);
+control.autoRotate = 2;
+
+//Adding stars to the browser
 
 function addStars(){
 
 const geom = new THREE.SphereGeometry(1,1,5,1);
-const mats = new THREE.MeshLambertMaterial({ color: 0x91a8d0});
+const mats = new THREE.MeshLambertMaterial({ color: 0xffffff});
 const star = new THREE.Mesh(geom, mats);
 
-const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(250));
+const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(450));
 
 star.position.set(x,y,z);
 scene.add(star)
-
-
-
 }
 
-const texture = new THREE.TextureLoader().load('chrome.jpg')
-scene.ball = texture;
+
 Array(500).fill().forEach(addStars)
 
+const spaceTexture = new THREE.TextureLoader().load('../assets/uni.jpg');
+scene.background = spaceTexture;
 
-const spacebg = new THREE.TextureLoader().load('milky.jpg')
-spacebg.wrapS = THREE.RepeatWrapping;
-spacebg.wrapT = THREE.RepeatWrapping;
+const sunText = new THREE.TextureLoader().load('../assets/sun.jpg');
+scene.ball = sunText;
+
+
+
 
 
 
 function animate(){
   requestAnimationFrame(animate);
 
-  ball.rotation.x += 0.00;
-  ball.rotation.y += 0.01;
-  ball.rotation.z =+ 0.00;
+  
 
+      ball.rotation.x += 0.00;
+      ball.rotation.y += 0.01;
+      ball.rotation.z += 0.00;
+      control.update();
+      
+      renderer.render(scene, camera);
 
-  renderer.render(scene, camera);
-
-  control.update()
+ 
 
 }
 

e86ea6e82f0dff12801d32c18de79800583ae8d2
JayOReilly <99806112+JayOReilly@users.noreply.github.com>
2022-09-07 23:13:46 +0100

    3rd Commit - Textures Applied to sphere

--- vanilla3d/main.js
+++ vanilla3d/main.js
@@ -2,69 +2,58 @@ import './style.css'
 import * as THREE from 'three';
 import { render } from '@react-three/fiber';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import CHROME from '../vanilla3d/assets/chrome.jpg';
+
 import { Stars } from '@react-three/drei';
 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
-
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(200);
+camera.position.setZ(300);
 renderer.render(scene, camera);
 
 
 
-
-/////////////////////////////////////////////
-
-
+// Sun
+//const sun = new THREE.TextureLoader().load('sun.jpg');
 const geom = new THREE.SphereGeometry(50,40,75);
-const mats = new THREE.MeshStandardMaterial
-    ({ });
-const ball = new THREE.Mesh(geom, mats);
-scene.add(ball)
+const mats = new THREE.MeshStandardMaterial({});
+const suntext = new THREE.TextureLoader().load('../assets/moon.jpg')
+const sun = new THREE.Mesh(
+  new THREE.SphereGeometry(50,40,75),
+  new THREE.MeshBasicMaterial({map: suntext})
+);
+const ball = new THREE.Mesh(geom, mats,sun);
+scene.ball = sun;
+scene.add(sun)
+
+
 
 ////////////////////////////////////
-
 const pointingLight = new THREE.PointLight(0xffffff)
-pointingLight.position.set(20,20,20)
-
+pointingLight.position.set(25,20,20)
 scene.add(pointingLight)
-
 const control = new OrbitControls(camera, renderer.domElement);
 control.autoRotate = 2;
 
+
+
 //Adding stars to the browser
-
 function addStars(){
-
 const geom = new THREE.SphereGeometry(1,1,5,1);
-const mats = new THREE.MeshLambertMaterial({ color: 0xffffff});
+const mats = new THREE.MeshLambertMaterial({});
 const star = new THREE.Mesh(geom, mats);
-
 const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(450));
-
 star.position.set(x,y,z);
 scene.add(star)
 }
-
-
 Array(500).fill().forEach(addStars)
-
 const spaceTexture = new THREE.TextureLoader().load('../assets/uni.jpg');
 scene.background = spaceTexture;
-
-const sunText = new THREE.TextureLoader().load('../assets/sun.jpg');
-scene.ball = sunText;
-
-
-
 
 
 
@@ -77,11 +66,7 @@ function animate(){
       ball.rotation.y += 0.01;
       ball.rotation.z += 0.00;
       control.update();
-      
       renderer.render(scene, camera);
-
- 
-
 }
 
 animate()
